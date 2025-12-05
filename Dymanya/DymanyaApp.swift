f20b4a7db73189f0b078db2a215c2e5228f9db83
Ahmed Shendy <dev.ahmed.shendy@gmail.com>
2025-12-05 14:37:54 +0200

    Initial Commit

---
+++ Dymanya/DymanyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DymanyaApp.swift
+//  Dymanya
+//
+//  Created by Ahmed Shendy on 05/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct DymanyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
